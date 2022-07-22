bd6f9c9ffb6a24ea7b11e93a4c404bcb8869d233
Alberto Sartori <alberto.sartori@huawei.com>
2022-07-22 12:46:52 +0200

    implement justbuild native remote protocol

--- pkg/cas/blob_access_directory_fetcher.go
+++ pkg/cas/blob_access_directory_fetcher.go
@@ -6,6 +6,7 @@ import (
 	remoteexecution "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
 	"github.com/buildbarn/bb-storage/pkg/blobstore"
 	"github.com/buildbarn/bb-storage/pkg/digest"
+	"github.com/buildbarn/bb-storage/pkg/justbuild"
 )
 
 type blobAccessDirectoryFetcher struct {
@@ -23,7 +24,21 @@ func NewBlobAccessDirectoryFetcher(blobAccess blobstore.BlobAccess, maximumMessa
 }
 
 func (df *blobAccessDirectoryFetcher) GetDirectory(ctx context.Context, digest digest.Digest) (*remoteexecution.Directory, error) {
-	m, err := df.blobAccess.Get(ctx, digest).ToProto(&remoteexecution.Directory{}, df.maximumMessageSizeBytes)
+	x := df.blobAccess.Get(ctx, digest)
+	is_just := digest.NewHasher().Size() == justbuild.NewBlobHasher().Size()
+	if is_just {
+		data, err := x.ToByteSlice(df.maximumMessageSizeBytes)
+		if err != nil {
+			return nil, err
+		}
+		m, err := justbuild.ToDirectoryMessage(data)
+		if err != nil {
+			return nil, err
+		}
+		return m, nil
+	}
+
+	m, err := x.ToProto(&remoteexecution.Directory{}, df.maximumMessageSizeBytes)
 	if err != nil {
 		return nil, err
 	}
